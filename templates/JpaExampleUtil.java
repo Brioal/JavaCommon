8ba4d97914adbc40badf1a0858c1e01bc57a82f8
brioal <brioal@foxmail.com>
2020-03-18 17:40:32 +0800

    添加JpaExampleUtil.java

---
+++ templates/JpaExampleUtil.java
@@ -0,0 +1,22 @@
+
+
+
+import org.springframework.data.domain.Example;
+
+/**
+ * 用于Jpa Example查询的工具
+ */
+public class JpaExampleUtil {
+
+
+    /**
+     * 返回简单的Example匹配,默认策略是只匹配非空的字段
+     *
+     * @param t
+     * @return
+     */
+    public static <T> Example<T> getSimpleExample(T t) {
+        Example<T> example = Example.of(t);
+        return example;
+    }
+}
